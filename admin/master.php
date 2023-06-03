72fd2e4a014ce4c116d61f934cf517536bfebfaf
milan <milanbharadva1@gmail.com>
2023-06-03 16:49:51 +0530

    simple set up

---
+++ admin/master.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>AdminLTE 3 | Dashboard</title>
+    <?php
+    include('./common/style.php')
+        ?>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+
+        <?php
+        include('./common/menu.php')
+            ?>
+
+        <?php
+        include('./common/sidebar.php')
+            ?>
+
+        <div class="content-wrapper">
+            <p>content goes here</p>
+        </div>
+
+        <?php
+        include('./common/footer.php')
+            ?>
+
+    </div>
+
+
+    <?php
+    include('./common/script.php')
+        ?>
+
+
+</body>
+
+</html>
